3a53efb9c8f4110f086ac6f2f4096a92f6eaca50
Dakouri M. Kobri <maurille.kobri@gmail.com>
2024-01-07 11:48:22 +0100

    Add first testimonial box

--- testimonial-grid/tailwind.config.js
+++ testimonial-grid/tailwind.config.js
@@ -1,6 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+
 export const content = ['./*.html'];
-export const theme = {
-  extend: {},
+
+export const screen = {
+  sm: '480px',
+  md: '768px',
+  lg: '976px',
+  xl: '1440px',
 };
+
+export const theme = {
+  extend: {
+    fontFamily: {
+      sans: ['Barlow Semi Condensed', 'sans-serif'],
+    },
+  },
+};
+
 export const plugins = [];
